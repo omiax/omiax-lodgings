30fb74b225b8e85f331697424c47dce1dd7847d8
Idy Udoh <idorenyinokonudoh@gmail.com>
2020-12-26 13:21:04 +0100

    Added initadmin management command

---
+++ user/management/commands/initadmin.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+
+from environ import Env
+
+env = Env()
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = 'Generate Su User, If none exist'
+
+    def handle(self, *args, **options):
+        admin_exist = User.objects.filter(is_superuser=True).count()
+        
+        if admin_exist == 0:
+            admin_user = User.objects.create_superuser(
+                username=env("DJANGO_SU_NAME"),
+                email=env("DJANGO_SU_EMAIL"),
+                password=env("DJANGO_SU_PASS"),
+                phone_number=env("DJANGO_SU_PHONE")
+            )
+            admin_user.save()
+
+            self.stdout.write(self.style.SUCCESS('Successfully added Superuser'))
+        else:
+            pass
