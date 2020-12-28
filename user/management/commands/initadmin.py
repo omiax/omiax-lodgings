1d220c3c94906725ef2f407bd9804bea9ebbf002
Idy Udoh <idorenyinokonudoh@gmail.com>
2020-12-28 15:54:58 +0100

    changed env variable names in initadmin.py

--- user/management/commands/initadmin.py
+++ user/management/commands/initadmin.py
@@ -11,14 +11,16 @@ class Command(BaseCommand):
     help = 'Generate Superuser, If none exist!'
 
     def handle(self, *args, **options):
-        admin_exist = User.objects.filter(is_superuser=True).count()
-        
+        admin_exist = User.objects.filter(
+            is_superuser=True
+        ).count()
+
         if admin_exist == 0:
             admin_user = User.objects.create_superuser(
-                username=env("DJANGO_SU_NAME"),
-                email=env("DJANGO_SU_EMAIL"),
-                password=env("DJANGO_SU_PASS"),
-                phone_number=env("DJANGO_SU_PHONE")
+                username=env("DJANGO_SUPERUSER_USERNAME"),
+                email=env("DJANGO_SUPERUSER_EMAIL"),
+                password=env("DJANGO_SUPERUSER_PASSWORD"),
+                phone_number=env("DJANGO_SUPERUSER_PHONE")
             )
             admin_user.save()
 
